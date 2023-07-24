825b7165ede8997a2d62be8171f351a2c9ea5215
Wesley Santos <wvsantos15@gmail.com>
2023-07-24 19:38:03 -0300

    Adição do método post

--- src/main/java/com/crud/crudspring/CrudSpringApplication.java
+++ src/main/java/com/crud/crudspring/CrudSpringApplication.java
@@ -1,12 +1,7 @@
 package com.crud.crudspring;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.crud.crudspring.model.Course;
-import com.crud.crudspring.repository.CourseRepository;
 
 @SpringBootApplication
 public class CrudSpringApplication {
@@ -14,20 +9,4 @@ public class CrudSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
-	
-
-	@Bean
-	CommandLineRunner initDatabase(CourseRepository courseRepository) {
-		return args -> {
-			courseRepository.deleteAll();
-
-			Course c = new Course();
-			c.setName("Angular");
-			c.setCategory("front-end");
-			
-			
-			courseRepository.save(c);
-		};
-	}
-
 }
--- src/main/java/com/crud/crudspring/controller/CourseController.java
+++ src/main/java/com/crud/crudspring/controller/CourseController.java
@@ -3,8 +3,14 @@ package com.crud.crudspring.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.crud.crudspring.model.Course;
@@ -24,5 +30,8 @@ public class CourseController {
 		
 	}
 	
-	
+	@PostMapping
+	public ResponseEntity<Course> create(@RequestBody Course course) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
+	}
 }
