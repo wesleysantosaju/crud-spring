2fc567e4805860b51a927773e446026cbd7a6f5e
Wesley Santos <wvsantos15@gmail.com>
2023-07-29 13:51:32 -0300

    Retirada de imports desnecessários na controller

--- src/main/java/com/crud/crudspring/controller/CourseController.java
+++ src/main/java/com/crud/crudspring/controller/CourseController.java
@@ -1,11 +1,7 @@
 package com.crud.crudspring.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,8 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.crud.crudspring.model.Course;
