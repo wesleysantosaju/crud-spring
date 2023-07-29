81cc284d501fa98ef364a0b07bab3698d6c46214
Wesley Santos <wvsantos15@gmail.com>
2023-07-29 13:48:50 -0300

    Implantação do método put

--- src/main/java/com/crud/crudspring/controller/CourseController.java
+++ src/main/java/com/crud/crudspring/controller/CourseController.java
@@ -2,7 +2,7 @@ package com.crud.crudspring.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,10 +26,11 @@ import com.crud.crudspring.repository.CourseRepository;
 @RequestMapping("/api/courses")
 public class CourseController {
 	
-	private List<Course> courses = new ArrayList<>();
-	
-	@Autowired
-	private CourseRepository courseRepository;
+	private final CourseRepository courseRepository;
+
+    public CourseController(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
 
 	@GetMapping
 	public List<Course> list(){
@@ -53,4 +55,19 @@ public class CourseController {
         return ResponseEntity.noContent().build();
     }
 	
+	@PutMapping("/edit/{id}")
+	public ResponseEntity<Course> edit(@PathVariable Long id, @RequestBody Course courseAt){
+		
+		 Course course = courseRepository.findById(id).get();
+
+	        if (course != null) {
+	            course.setName(courseAt.getName());
+	            course.setCategory(courseAt.getCategory());
+
+	            Course updatedCourseData = courseRepository.save(course);
+	            return new ResponseEntity<>(updatedCourseData, HttpStatus.OK);
+	        } else {
+	            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	        }
+	    }
 }
